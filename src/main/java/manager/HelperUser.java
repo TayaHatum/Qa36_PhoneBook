c08f4d5615a7d35bfcbb357b3b499c5edfb7d5bc
TayaHatum <21taya21@gmail.com>
2023-01-10 21:21:37 +0200

    login

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,7 +1,11 @@
 package manager;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class HelperUser extends HelperBase{
     public HelperUser(WebDriver wd) {
@@ -9,19 +13,51 @@ public class HelperUser extends HelperBase{
     }
 
     public void openLoginRegistrationForm(){
-        click(By.cssSelector(""));
+        click(By.cssSelector("a[href='/login']"));
     }
 
     public void fillLoginRegistrationForm(String email,String password){
         // for email
-        type(By.cssSelector(""),email);
+        type(By.name("email"),email);
         // for password
-        type(By.cssSelector(""),password);
+        type(By.name("password"),password);
 
     }
 
     public void submitLogin(){
-        click(By.cssSelector(""));
+        click(By.cssSelector("[name='login']"));
 
+    }
+
+    public boolean isLogged() {
+//        try {
+//            wd.findElement(By.xpath("//button[text()='Sign Out']")).isDisplayed();
+//            return true;
+//        }catch (Exception e){
+//            return false;
+//        }
+       List<WebElement> list  = wd.findElements(By.xpath("//button[text()='Sign Out']"));
+       return list.size() > 0;
+    }
+
+    public void logout() {
+        click(By.xpath("//button[text()='Sign Out']"));
+    }
+
+    public boolean isErrorMessageDisplayed(String message) {
+       Alert alert = wd.switchTo().alert();
+       String text = alert.getText();
+        System.out.println(text);
+
+        // click ok
+        alert.accept();
+        //click cancel
+        //alert.dismiss();
+        //alert.sendKeys("Hello");
+        return text.contains(message);
+    }
+
+    public void submitRegistration() {
+        click(By.cssSelector("[name='registration']"));
     }
 }
