45e97877143ff533948dd3b028962a8617826248
TayaHatum <21taya21@gmail.com>
2023-01-31 20:05:25 +0200

    from file

--- src/main/java/manager/DataProviderUser.java
+++ src/main/java/manager/DataProviderUser.java
@@ -3,6 +3,7 @@ package manager;
 import model.User;
 import org.testng.annotations.DataProvider;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -34,9 +35,15 @@ public class DataProviderUser {
 
     }
     @DataProvider
-
-    public Iterator<Object[]> loginDataUserFromFile(){
+    public Iterator<Object[]> loginDataUserFromFile() throws IOException {
         List<Object[]> list = new ArrayList<>();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("src/test/resources/data.csv")));
+        String  line =bufferedReader.readLine();  // "noa@gmail.com,Nnoa12345$"
+        while(line!=null){
+            String[] split = line.split(","); // [0] "noa@gmail.com" [1] "Nnoa12345$"
+            list.add(new Object[]{User.builder().email(split[0]).password(split[1]).build()});
+            line =bufferedReader.readLine(); /// "sonya@gmail.com,Ss12345$"
+         }
 
         return list.iterator();
     }
